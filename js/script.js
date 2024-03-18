795390ed7a3bcc7260b026842d7259b79656184c
ArDevPro <webwm@mail.ru>
2024-03-19 00:05:16 +0200

    callback fn and uncomments all code

--- js/script.js
+++ js/script.js
@@ -11,46 +11,46 @@
 // PascalCase
 
 // интерполяция
-// const nameuser = "Alex";
-// console.log(`Привет ${nameuser}`);
+const nameuser = "Alex";
+console.log(`Привет ${nameuser}`);
 
 // унарный плюс
-// const testnum = 5;
-// const strtest = +"5"; // превращяет тип строки в число или NaN (Not-A-Number)
-// console.log(testnum + strtest);
+const testnum = 5;
+const strtest = +"5"; // превращяет тип строки в число или NaN (Not-A-Number)
+console.log(testnum + strtest);
 
 // префиксный постфиксный инкремент
-// let incr = 10,
-// 	decr = 10;
+let incr = 10,
+	decr = 10;
 
 // постфиксный
-// console.log(incr++); // 10
-// console.log(decr--); // 10
-// // и только потом они становятся 11 и 9
+console.log(incr++); // 10
+console.log(decr--); // 10
+// и только потом они становятся 11 и 9
 
 // префиксный
-// console.log(++incr); // 11
-// console.log(--decr); // 9
+console.log(++incr); // 11
+console.log(--decr); // 9
 
 
 // остаток от деления 
-// console.log(5%2); // 1
+console.log(5%2); // 1
 
 // строгое сранвение
-// console.log(2*4 == "8"); // == true даже строку и число 
-// console.log(2*4 === "8"); // === false строка не является числом NaN
+console.log(2*4 == "8"); // == true даже строку и число 
+console.log(2*4 === "8"); // === false строка не является числом NaN
 
 // пример сравнений
 // Выполняется ли условие?
-// const hamburger = 3;
-// const fries = 3;
-// const cola = 0;
-// const nuggets = 2;
+const hamburger = 3;
+const fries = 3;
+const cola = 0;
+const nuggets = 2;
 
 
-// if (hamburger === 3 && cola || fries === 3 && nuggets) {
-// 	console.log("Done!");
-// }
+if (hamburger === 3 && cola || fries === 3 && nuggets) {
+	console.log("Done!");
+}
 
 
 
@@ -61,87 +61,98 @@
 // *******
 //*********
 
-// const lineStars = 6;
-// const plusStars = 2;
-// let result = "";
-// let startStars = 1;
+const lineStars = 6;
+const plusStars = 2;
+let result = "";
+let startStars = 1;
 
-// for (let i = lineStars; i >= 2; i--) {
+for (let i = lineStars; i >= 2; i--) {
 	 
-// 	for (let space = 0; space < i; space++) {
+	for (let space = 0; space < i; space++) {
 		
-// 		result += " ";
+		result += " ";
 
-// 		if(space === (i-2)){
+		if(space === (i-2)){
 			
 			
-// 			for (let s = 0; s < startStars; s++) {
-// 				result += "*";
-// 			}
-// 			result += "\n";
-// 			startStars += plusStars;
+			for (let s = 0; s < startStars; s++) {
+				result += "*";
+			}
+			result += "\n";
+			startStars += plusStars;
 
-// 			break;
-// 		} 
-// 	}
+			break;
+		} 
+	}
 
 	
-// }
-// for (let s = 0; s < startStars; s++) {
-// 	result += "*";
-// }
-// result += "\n";
-// console.log(result);
+}
+for (let s = 0; s < startStars; s++) {
+	result += "*";
+}
+result += "\n";
+console.log(result);
 
 
-// // Место для третьей задачи
-// function getMathResult(a, b) {
-// 	if(typeof(b) == "number" && b > 0){
-// 		let plusnum = a; 
-// 		let res = "";
+// Место для третьей задачи
+function getMathResult(a, b) {
+	if(typeof(b) == "number" && b > 0){
+		let plusnum = a; 
+		let res = "";
 		
-// 		for (var i = 0; i < b; i++) {
+		for (var i = 0; i < b; i++) {
 			
-// 			if(i != b-1){
-// 				res += plusnum + "---";
-// 			}else{
-// 				res += plusnum;
-// 			}
-// 			plusnum += a;           
-// 		}
-// 		return res;
-// 	}else{
-// 		return a;
-// 	}
-// }
+			if(i != b-1){
+				res += plusnum + "---";
+			}else{
+				res += plusnum;
+			}
+			plusnum += a;           
+		}
+		return res;
+	}else{
+		return a;
+	}
+}
 
-// console.log( getMathResult(5, 3) );
+console.log( getMathResult(5, 3) );
 
 // динамическое программирование снизу вверх.
-// function fib(num) {
-// 	if (typeof(num) !== "number" || num <= 0 || !Number.isInteger(num)) {
-// 		return "";
-// 	}
+function fib(num) {
+	if (typeof(num) !== "number" || num <= 0 || !Number.isInteger(num)) {
+		return "";
+	}
 
-// 	let result = "";
-// 	let first = 0;
-// 	let second = 1;
+	let result = "";
+	let first = 0;
+	let second = 1;
 
-// 	for (let i = 0; i < num; i++) {
-// 		if (i + 1 === num) {
-// 			result += `${first}`;
-// 			// Без пробела в конце
-// 		} else {
-// 			result += `${first} `;
-// 		}
+	for (let i = 0; i < num; i++) {
+		if (i + 1 === num) {
+			result += `${first}`;
+			// Без пробела в конце
+		} else {
+			result += `${first} `;
+		}
 
-// 		let third = first + second;
-// 		first = second;
-// 		second = third;
-// 	}
+		let third = first + second;
+		first = second;
+		second = third;
+	}
 
-// 	return result;
-// }
- 
+	return result;
+}
 
-// console.log(fib(7));
+console.log(fib(7));
+
+// callback fn
+function learnJS(lang, callback){
+	console.log(`Я учу: ${lang}`);
+	callback();
+}
+
+function done() {
+	console.log("Я прошел этот урок!");
+}
+
+learnJS("JavaScript", done);
